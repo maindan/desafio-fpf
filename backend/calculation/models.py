018cb69bb9379f453286bebb954f2341c438bb59
Dan <danielkksilva1@gmail.com>
2025-04-05 09:35:55 -0400

    feat: adicionado model de Processamento e migration

--- backend/calculation/models.py
+++ backend/calculation/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Processing(models.Model):
+    num1 = models.FloatField()
+    num2 = models.FloatField()
+    num3 = models.FloatField()
+    status = models.TextField(max_length=20)
+    media = models.FloatField(null=True)
+    mediana = models.FloatField(null=True)
